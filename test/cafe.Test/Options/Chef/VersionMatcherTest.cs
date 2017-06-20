9f27b437209e4233728f58e58925f911845e2ddd
Michael Hedgpeth <mhedgpeth@gmail.com>
2017-06-19 20:11:14 -0500

    Made version match more flexible, do not reinstall if version is the same

---
+++ test/cafe.Test/Options/Chef/VersionMatcherTest.cs
@@ -0,0 +1,39 @@
+﻿using cafe.Options.Chef;
+using cafe.Shared;
+using FluentAssertions;
+using Xunit;
+
+namespace cafe.Test.Options.Chef
+{
+    public class VersionMatcherTest
+    {
+        [Fact]
+        public void DoVersionsMatch_ShouldBeTrueWhenMatching()
+        {
+            var version = "12.2.1";
+            VersionMatcher.DoVersionsMatch(version, version).Should()
+                .BeTrue("because the versions exactly match");
+        }
+
+        [Fact]
+        public void DoVersionsMatch_ShouldBeFalseWhenDifferent()
+        {
+            VersionMatcher.DoVersionsMatch("12.2.1", "12.2.2").Should()
+                .BeFalse("because the versions are different");
+        }
+
+        [Fact]
+        public void DoVersionsMatch_ShouldBeTrueWhenLastDigitDifferent()
+        {
+            VersionMatcher.DoVersionsMatch("12.2.1", "12.2.1.1").Should()
+                .BeTrue("because only the last digit is different");
+        }
+
+        [Fact]
+        public void DoVersionsMatch_ShouldBeFalseWhenFirstDigitDifferentAndLastDigitDifferent()
+        {
+            VersionMatcher.DoVersionsMatch("13.2.1", "12.2.1.1").Should()
+                .BeFalse("because first digit is different");
+        }
+    }
+}
